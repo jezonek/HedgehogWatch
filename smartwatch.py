4e80ff69cdb8e337daf8193784982e02ff92f5ae
jezonek <marcinjezon@gmail.com>
2019-01-06 00:12:39 +0100

    Finished small refactor

--- smartwatch.py
+++ smartwatch.py
@@ -3,7 +3,6 @@ import datetime
 import logging
 
 from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
 from luma.core.virtual import sevensegment
 from luma.led_matrix.device import max7219
 
@@ -16,10 +15,13 @@ logging.info('Started app')
 
 
 def prepare_time_for_display(hour, minute, second):
-    return 't {hour}.{minute}.{second}'.format(hour=str(hour).zfill(2), minute=str(minute).zfill(2), second=str(second).zfill(2))
+    return 't {hour}.{minute}.{second}'.format(hour=str(hour).zfill(2), minute=str(minute).zfill(2),
+                                               second=str(second).zfill(2))
 
-def prepare_date_for_display(day, month, year)
-    return pass
+
+def prepare_date_for_display(day, month, year):
+    return '{day}.{month}.{year}'.format(day=str(day).zfill(2), month=str(month).zfill(2), year=str(year))
+
 
 while (True):
     # TODO set time format from datetme library, next try to send systemtime to watch
@@ -27,15 +29,15 @@ while (True):
     try:
         for second in range(10):
             for second in range(30):
-                czas = datetime.datetime.now()
-                komunikat = prepare_time_for_display(czas.hour, czas.minute, czas.second)
-                display.text = komunikat
+                current_time = datetime.datetime.now()
+                statement = prepare_time_for_display(current_time.hour, current_time.minute, current_time.second)
+                display.text = statement
                 time.sleep(1)
-            data = datetime.datetime.now()
-            komunikat = str(data.day).zfill(2) + '.' + str(data.month).zfill(2) + '.' + str(data.year)
-            display.text = str(komunikat)
+            current_date = datetime.datetime.now()
+            statement = prepare_date_for_display(current_date.day, current_date.month, current_date.year)
+            display.text = str(statement)
             time.sleep(10)
     except Exception as e:
         logging.critical('Critical exception: {}'.format(e.message))
-        komunikat = 'Error'
-        display.text = str(komunikat)
+        statement = 'Error'
+        display.text = str(statement)
